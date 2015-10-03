3a19c919a3306e10fbca2970b5440e6cedc46796
TannerEady <tanneready@shaw.ca>
2015-10-03 20:26:12 +0000

    Create three mathematical methods.
    Add, subtract, or multiply two numbers, return the result.

--- two_player_math_game.rb
+++ two_player_math_game.rb
@@ -3,5 +3,21 @@ def random_num
   rand(1..20)
 end
 
+# Takes in and adds two numbers, returns sum.
+def add(a, b)
+  a + b
+end
 
+# Takes in and subtracts two numbers, returns difference.
+def subtract(a, b)
+  a - b
+end
 
+# Takes in and multiplies two numbers, returns product.
+def multiply(a, b)
+  a * b
+end
+
+puts add(3, 4)
+puts subtract(5, 3)
+puts multiply(3, 2)
